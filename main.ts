19f535c57151f2e7ae05ac46509d13f82ce883f2
Thomas Ghysels <info@thomasg.be>
2021-10-27 15:03:29 +0200

    broadcast

--- main.ts
+++ main.ts
@@ -3,30 +3,43 @@ import { Application, Router, Context } from "https://deno.land/x/oak/mod.ts";
 
 const rooms = {}
 
-
-// const router = new Router();
+const region = Deno.env.get("DENO_REGION")
+const channel = new BroadcastChannel('main');
 
 // Serve static content
-const index =  Deno.readTextFile(`${Deno.cwd()}/index.html`);
-const connectable =  Deno.readTextFile(`${Deno.cwd()}/connectable.js`);
+const index = Deno.readTextFile(`${Deno.cwd()}/index.html`);
+const connectable = Deno.readTextFile(`${Deno.cwd()}/connectable.js`);
 
 console.log('index use')
 
-const app = new Application();
 // app.use(router.routes());
 // app.use(router.allowedMethods());
 
-let i = 1
-app.use(async (ctx: Context) => {
-  const id = i++
+
+
+
+// Listen to other regions
+channel.addEventListener('message', (evt) => {
+  console.log('received', evt.data)
+  const { data, roomName, region } = evt.data
+
+  // Broadcast external messages to this region
+  rooms[roomName].forEach(peer => {
+    peer.socket.send(data)
+  })
+})
+
+// Listen to clients
+app.const app = new Application();
+se(async (ctx: Context) => {
   console.log('index')
   if (!ctx.isUpgradable) {
     console.log('index serve', ctx.request.pathname)
-    if (ctx.request.url.pathname==='/connectable.js') {
+    if (ctx.request.url.pathname === '/connectable.js') {
       ctx.response.type = 'application/javascript'
       ctx.response.body = await connectable
     } else {
-      ctx.response.body = String(await index).replaceAll ('Listening', 'Listening in ' + Deno.env.get("DENO_REGION"))
+      ctx.response.body = String(await index).replaceAll('Listening', 'Listening in ' + region)
     }
     return
   }
@@ -41,26 +54,31 @@ app.use(async (ctx: Context) => {
   }
 
   // Wait for open socket
-  const socket:WebSocket = await ctx.upgrade()
+  const socket: WebSocket = await ctx.upgrade()
   await new Promise(res => {
     socket.addEventListener('open', res)
   })
 
   // Save for later
-  rooms[roomName].push({ id, socket})
+  rooms[roomName].push({ socket })
   console.log(roomName, rooms[roomName].length, 'subs')
 
   // Broadcast messages
   socket.addEventListener('message', (evt) => {
     console.log(roomName, 'message', evt.data.slice(0, 80))
+
+    // This region
     rooms[roomName].forEach(peer => {
       if (socket !== peer.socket) {
         peer.socket.send(evt.data)
       }
     })
+
+    // Other regions
+    channel.postMessage({ region, roomName, data: evt.data })
   })
 
-  socket.addEventListener('close', function() {
+  socket.addEventListener('close', function () {
     console.log(roomName, rooms[roomName].length, 'subs', rooms[roomName].map(s => s.socket.readyState))
     rooms[roomName] = rooms[roomName].filter(s => s.socket !== socket)
     console.log(roomName, rooms[roomName].length, 'subs', rooms[roomName].map(s => s.socket.readyState))
